84bca2f75522723da1ee1f236b822800658555cf
Jon-Felipe <rodriguesjf7@gmail.com>
2023-03-19 10:33:30 +0000

    styled header for smaller screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useMediaQuery from '../hooks/useMediaQuery';
+
+// components
 import NavLink from './NavLink';
 
 const Header = () => {
+  const isAboveMediumScreen = useMediaQuery('(min-width: 1060px)');
+
   return (
     <header className='h-20 flex items-center justify-between shadow'>
-      <div className='flex items-center justify-between w-full max-w-screen-lg mx-auto'>
+      <div className='flex items-center justify-between w-11/12 max-w-screen-lg mx-auto'>
         <h1 className='text-3xl font-bold tracking-wide'>Expense Tracker</h1>
 
-        <nav>
-          <ul className='flex items-center gap-6'>
-            <NavLink text='Home' />
-            <NavLink text='About' />
-            <NavLink text='Contact' />
-          </ul>
-        </nav>
+        {isAboveMediumScreen ? (
+          <nav>
+            <ul className='flex items-center gap-6'>
+              <NavLink text='Home' />
+              <NavLink text='About' />
+              <NavLink text='Contact' />
+            </ul>
+          </nav>
+        ) : (
+          <div>
+            <p className='text-4xl font-bold cursor-pointer'>x</p>
+          </div>
+        )}
       </div>
     </header>
   );
