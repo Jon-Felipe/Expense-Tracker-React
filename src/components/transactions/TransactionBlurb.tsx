da21fea8f5732416f0c5bd941b771d4b2ff6ab72
Jon-Felipe <rodriguesjf7@gmail.com>
2023-03-19 15:55:10 +0000

    created transaction blurb component

---
+++ src/components/transactions/TransactionBlurb.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const TransactionBlurb = (props: Props) => {
+  return <div>TransactionBlurb</div>;
+};
+
+export default TransactionBlurb;
--- src/components/transactions/index.tsx
+++ src/components/transactions/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
+// components
+import TransactionBlurb from './TransactionBlurb';
+
+// dummy data
+import { transactions } from '../../utils/transactions';
+
 type Props = {};
 
 const Transactions = (props: Props) => {
-  return <div>Transactions</div>;
+  return (
+    <section>
+      {transactions.map((transaction) => (
+        <TransactionBlurb key={transaction.id} />
+      ))}
+    </section>
+  );
 };
 
 export default Transactions;
