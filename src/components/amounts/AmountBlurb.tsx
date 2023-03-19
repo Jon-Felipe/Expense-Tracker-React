75332e618d4acbf7a31c913ec6d538538c686520
Jon-Felipe <rodriguesjf7@gmail.com>
2023-03-19 15:25:51 +0000

    styled amount blurbs

--- src/components/amounts/AmountBlurb.tsx
+++ src/components/amounts/AmountBlurb.tsx
@@ -3,13 +3,34 @@ import React from 'react';
 type Props = {
   type: string;
   value: string;
+  icon: React.ReactNode;
+  typeColour?: string;
+  bgColour?: string;
+  valueColour?: string;
 };
 
-const AmountBlurb = ({ type, value }: Props) => {
+const AmountBlurb = ({
+  type,
+  value,
+  icon,
+  typeColour,
+  bgColour,
+  valueColour,
+}: Props) => {
   return (
-    <div className='bg-white w-60 h-28 rounded-xl flex flex-col items-center justify-center gap-2 shadow'>
-      <h3 className='text-sm font-bold tracking-wide text-gray-400'>{type}</h3>
-      <p className='text-2xl'>${value}</p>
+    <div
+      className={`w-60 h-24 rounded-xl flex items-center justify-evenly gap-2 shadow ${
+        bgColour ? bgColour : 'bg-white'
+      }`}
+    >
+      <div className='p-1 bg-white rounded-lg'>{icon}</div>
+
+      <div>
+        <h3 className={`text-base font-bold tracking-wide ${typeColour}`}>
+          {type}
+        </h3>
+        <p className={`text-3xl ${valueColour}`}>${value}</p>
+      </div>
     </div>
   );
 };
--- src/components/amounts/index.tsx
+++ src/components/amounts/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FiArrowDown, FiArrowUp } from 'react-icons/fi';
+import { FaMoneyBill } from 'react-icons/fa';
 
 // components
 import AmountBlurb from './AmountBlurb';
@@ -7,10 +9,29 @@ type Props = {};
 
 const Amounts = (props: Props) => {
   return (
-    <section className='flex items-center justify-between w-11/12 max-w-screen-lg mx-auto py-10'>
-      <AmountBlurb type='Account Balance' value='9400' />
-      <AmountBlurb type='Income' value='5000' />
-      <AmountBlurb type='Expenses' value='1200' />
+    <section className='flex items-center justify-between w-5/12 max-w-screen-lg mx-auto py-10'>
+      <AmountBlurb
+        type='Account Balance'
+        value='9400'
+        icon={<FaMoneyBill className='w-10 h-10' />}
+        typeColour='text-gray-400'
+      />
+      <AmountBlurb
+        type='Income'
+        value='5000'
+        icon={<FiArrowDown className='w-10 h-10 text-green-500' />}
+        typeColour='text-white'
+        bgColour='bg-green-500'
+        valueColour='text-white'
+      />
+      <AmountBlurb
+        type='Expenses'
+        value='1200'
+        icon={<FiArrowUp className='w-10 h-10 text-red-500' />}
+        typeColour='text-white'
+        bgColour='bg-red-500'
+        valueColour='text-white'
+      />
     </section>
   );
 };
