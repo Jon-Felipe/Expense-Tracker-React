1b29400a4043436cdf99f56f0b109b6c5023b646
Jon-Felipe <rodriguesjf7@gmail.com>
2023-03-19 15:40:08 +0000

    created recent transactions component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import Amounts from './components/amounts';
 
 // components
 import Header from './components/Header';
+import Amounts from './components/amounts';
+import RecentTransactions from './components/RecentTransactions';
 
 function App() {
   return (
@@ -10,8 +11,11 @@ function App() {
       <Header />
 
       <main className=' bg-gray-50 h-screen'>
-        {/* Amounts Blurbs */}
+        {/* Amounts */}
         <Amounts />
+
+        {/* Recent Transactions */}
+        <RecentTransactions />
       </main>
     </>
   );
---
+++ src/components/RecentTransactions.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const RecentTransactions = (props: Props) => {
+  return <section>RecentTransactions</section>;
+};
+
+export default RecentTransactions;
