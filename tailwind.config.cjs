90ce84a1139a705791f058b69ae932ab3795ee64
Jon-Felipe <rodriguesjf7@gmail.com>
2023-03-19 10:24:23 +0000

    added customized screen sizes to tailwind config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,11 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    screens: {
+      xs: '480px',
+      sm: '768px',
+      md: '1060px',
+    },
   },
   plugins: [],
 };
