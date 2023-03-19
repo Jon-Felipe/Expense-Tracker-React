0b812dda50b2f319ecc94683064db67b4658c01d
Jon-Felipe <rodriguesjf7@gmail.com>
2023-03-19 15:50:23 +0000

    created dummy transactions

---
+++ src/utils/transactions.ts
@@ -0,0 +1,32 @@
+export const transactions = [
+  {
+    id: 1,
+    transaction: {
+      type: 'Expense',
+      item: 'Shopping',
+      description: 'Bought some groceries',
+      amount: 120,
+      time: '10:00 AM',
+    },
+  },
+  {
+    id: 2,
+    transaction: {
+      type: 'Expense',
+      item: 'Subscription',
+      description: 'Paid monthly Netflix subscription',
+      amount: 9,
+      time: '04:30 PM',
+    },
+  },
+  {
+    id: 3,
+    transaction: {
+      type: 'Income',
+      item: 'Monthly Salary',
+      description: 'Recieved monthly salary for June',
+      amount: 5500,
+      time: '11:30 AM',
+    },
+  },
+];
