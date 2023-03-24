16af4432bb181c35615b1fadbc511500a18b42ed
Jon-Felipe <rodriguesjf7@gmail.com>
2023-03-24 19:48:58 +0000

    basic modal styling

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaTimes } from 'react-icons/fa';
 
 type Props = {
   setModalIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -10,20 +11,40 @@ const Modal = ({ setModalIsOpen }: Props) => {
   };
 
   return (
-    <div className='w-screen h-screen bg-gray-200 fixed flex items-center justify-center'>
+    <div className='w-screen h-screen bg-gray-100 fixed flex items-center justify-center'>
       {/* Container */}
-      <div className='w-1/2 h-2/3 rounded-lg bg-white shadow flex flex-col p-6'>
-        {/* Close Button */}
-        <button onClick={handleCloseModal}> X </button>
-        {/* Title */}
-        <div>
-          <h1>Add your transaction here</h1>
+      <div className='w-1/3 h-1/2 rounded-lg bg-white shadow flex flex-col p-8'>
+        <div className='flex items-center justify-between'>
+          {/* Title */}
+          <div>
+            <h1 className='font-bold text-3xl'>Transaction Details</h1>
+          </div>
+          {/* Close Button */}
+          <button
+            onClick={handleCloseModal}
+            className='bg-slate-50 p-2 rounded-md shadow'
+          >
+            <FaTimes className='h-6 w-6' />
+          </button>
         </div>
         {/* Body */}
-        <div></div>
+        <div className='h-full'>
+          <div>
+            <input type='text' placeholder='placeholder text' />
+          </div>
+          <div>
+            <input type='text' placeholder='placeholder text' />
+          </div>
+          <div>
+            <input type='text' placeholder='placeholder text' />
+          </div>
+          <div>
+            <input type='text' placeholder='placeholder text' />
+          </div>
+        </div>
         {/* Footer */}
-        <div>
-          <button>Cancel</button>
+        <div className='flex justify-end gap-4'>
+          <button onClick={handleCloseModal}>Cancel</button>
           <button>Add Transaction</button>
         </div>
       </div>
