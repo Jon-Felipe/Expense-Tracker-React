8ba612a872061c243f281430864a2fc41ffc45f3
Jon-Felipe <rodriguesjf7@gmail.com>
2023-03-26 16:54:38 +0100

    styled textarea component

--- src/components/ui/TextArea.tsx
+++ src/components/ui/TextArea.tsx
@@ -3,7 +3,22 @@ import React from 'react';
 type Props = {};
 
 const TextArea = (props: Props) => {
-  return <div>TextArea</div>;
+  return (
+    <div>
+      <label
+        htmlFor='description'
+        className='block mb-2 text-sm font-medium text-gray-900'
+      >
+        Description
+      </label>
+      <textarea
+        name='description'
+        id='description'
+        className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300'
+        placeholder='Write your description here...'
+      ></textarea>
+    </div>
+  );
 };
 
 export default TextArea;
