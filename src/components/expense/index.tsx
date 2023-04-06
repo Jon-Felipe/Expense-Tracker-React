38fb6f69d0904f332fdae4a212fee91be9e62e19
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-06 19:56:42 +0100

    added income and expense html values

--- src/components/expense/index.tsx
+++ src/components/expense/index.tsx
@@ -3,7 +3,26 @@ import React from 'react';
 type Props = {};
 
 const Expense = (props: Props) => {
-  return <div>Expense</div>;
+  return (
+    <article>
+      <h3>Add New Expense</h3>
+      <div>
+        <div>
+          <h5>Expense Name</h5>
+          <input type='text' />
+        </div>
+        <div>
+          <h5>Amount</h5>
+          <input type='number' />
+        </div>
+      </div>
+      <div>
+        <h5>Budget Category</h5>
+        <input type='string' />
+      </div>
+      <button>Create Button</button>
+    </article>
+  );
 };
 
 export default Expense;
--- src/components/income/index.tsx
+++ src/components/income/index.tsx
@@ -3,7 +3,20 @@ import React from 'react';
 type Props = {};
 
 const Income = (props: Props) => {
-  return <div>Income</div>;
+  return (
+    <article>
+      <h3>Insert Income Value</h3>
+      <div>
+        <h5>Income Name</h5>
+        <input type='number' />
+      </div>
+      <div>
+        <h5>Amount</h5>
+        <input type='number' />
+      </div>
+      <button>Create Button</button>
+    </article>
+  );
 };
 
 export default Income;
