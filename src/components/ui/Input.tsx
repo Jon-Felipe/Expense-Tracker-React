a470115c91f1841c7bddb6935a73b05919ae29d6
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-09 20:25:03 +0100

    created basic input with label and added props and types

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const Input = () => {
-  return <div>Input</div>;
+type Props = {
+  LabelText: string;
+  type: React.HTMLInputTypeAttribute;
+  name: string | undefined;
+  value: string | number | readonly string[] | undefined;
+  onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
+};
+
+const Input = ({ LabelText, type, name, value, onChange }: Props) => {
+  return (
+    <div>
+      <label htmlFor=''>{LabelText}</label>
+      <input type={type} name={name} value={value} onChange={onChange} />
+    </div>
+  );
 };
 
 export default Input;
