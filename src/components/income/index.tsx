bfaa499455ebf7af622906c71f5da2b9f9dfd01e
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-09 20:15:15 +0100

    updated income form text and placeholders

--- src/components/income/index.tsx
+++ src/components/income/index.tsx
@@ -6,12 +6,12 @@ type Props = {};
 const IncomeForm = (props: Props) => {
   return (
     <article className='w-full border-2 border-dashed px-6 py-4'>
-      <h3 className='text-2xl font-bold'>Insert Income Value</h3>
+      <h3 className='text-2xl font-bold'>Add Income</h3>
       <div className='my-2'>
         <h5 className='text-lg font-semibold'>Income Name</h5>
         <input
           type='number'
-          placeholder='e.g. Groceries'
+          placeholder='e.g. Salary'
           className='w-full border-2 px-4 py-2 rounded-lg'
         />
       </div>
@@ -19,7 +19,7 @@ const IncomeForm = (props: Props) => {
         <h5 className='text-lg font-semibold'>Amount</h5>
         <input
           type='number'
-          placeholder='$350'
+          placeholder='$5500'
           className='w-full border-2 px-4 py-2 rounded-lg'
         />
       </div>
