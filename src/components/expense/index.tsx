e79100654c8890b5fb08dffc108b8c5d1db4219a
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-18 19:48:39 +0100

    added hanlde submit function placholder and changed to form

--- src/components/expense/index.tsx
+++ src/components/expense/index.tsx
@@ -26,8 +26,15 @@ const ExpenseForm = ({ setTransactions }: Props) => {
     });
   };
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
-    <article className='w-full border-2 border-dashed px-6 py-4'>
+    <form
+      className='w-full border-2 border-dashed px-6 py-4'
+      onSubmit={handleSubmit}
+    >
       <h3 className='text-2xl font-bold'>Add New Expense</h3>
       <div>
         <div className='my-2'>
@@ -53,13 +60,16 @@ const ExpenseForm = ({ setTransactions }: Props) => {
           />
         </div>
       </div>
-      <button className='flex items-center gap-2 bg-gray-900 text-white px-4 py-2 rounded-lg'>
+      <button
+        type='submit'
+        className='flex items-center gap-2 bg-gray-900 text-white px-4 py-2 rounded-lg'
+      >
         Create Expense
         <span>
           <BiDollarCircle className='w-6 h-6' />
         </span>
       </button>
-    </article>
+    </form>
   );
 };
 
