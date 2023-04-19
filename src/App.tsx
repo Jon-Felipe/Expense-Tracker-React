5f7440881347a9cc6a1ba6217e325a1243331d3f
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-19 10:04:53 +0100

    created expense form component

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ import Header from './components/Header';
 import Amounts from './components/amounts';
 import Transactions from './components/transactions';
 import IncomeForm from './components/income';
-import ExpenseForm from './components/expense';
+// import ExpenseForm from './components/expense';
+import ExpenseForm from './components/ExpenseForm';
 
 // extras
 import { ITransactionType, IAmountValues, IIncomeValues } from './utils/types';
@@ -42,13 +43,20 @@ const App = () => {
           expense={amountValues.expenses}
         />
 
+        <div>
+          {/* Income Form */}
+          <ExpenseForm />
+          {/* Expense Form */}
+          <ExpenseForm />
+        </div>
+
         <div className='max-w-screen-lg mx-auto px-4 flex flex-col gap-4 md:flex-row'>
-          <IncomeForm
+          {/* <IncomeForm
             initialIncomeValues={incomeValues}
             setIncomeValues={setIncomeValues}
             setTransactions={setTransactions}
-          />
-          <ExpenseForm setTransactions={setTransactions} />
+          /> */}
+          {/* <ExpenseForm setTransactions={setTransactions} /> */}
         </div>
 
         {/* Transactions */}
