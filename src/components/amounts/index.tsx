61429d951a1f5bd91e3600ea36d6c65d71dbf866
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-20 12:02:13 +0100

    total calculation

--- src/components/amounts/index.tsx
+++ src/components/amounts/index.tsx
@@ -14,12 +14,13 @@ type Props = {
 const Amounts = ({ transactions }: Props) => {
   const income: number = calculateTotal(transactions, 'income');
   const expense: number = calculateTotal(transactions, 'expense');
+  const total = income - expense;
 
   return (
     <section className='flex flex-col gap-4 md:flex-row my-10'>
       <AmountBlurb
         type='Account Balance'
-        value={income - expense}
+        value={total}
         icon={<FaMoneyBill className='w-10 h-10' />}
         typeColour='text-gray-400'
       />
